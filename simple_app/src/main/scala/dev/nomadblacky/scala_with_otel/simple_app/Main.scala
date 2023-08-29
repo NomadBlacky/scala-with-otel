672e747900e69daa574ca635fbdd8902086f3731
Takumi Kadowaki <takumi.kadowaki@3-shake.com>
2023-08-29 18:51:44 +0900

    feat: Add some trace operations

--- simple_app/src/main/scala/dev/nomadblacky/scala_with_otel/simple_app/Main.scala
+++ simple_app/src/main/scala/dev/nomadblacky/scala_with_otel/simple_app/Main.scala
@@ -4,7 +4,10 @@ import io.opentelemetry.api.GlobalOpenTelemetry
 import io.opentelemetry.api.trace.{Span, StatusCode}
 import io.opentelemetry.context.Context
 
+import scala.concurrent.{Await, Future}
 import scala.util.Using
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
 
 object Main:
 
@@ -31,10 +34,18 @@ object Main:
           .setAttribute("heavy-task-duration-seconds", heavyTaskDurationSeconds)
           .startSpan()
 
+        val future = futureTask()
+
         Using(heavyTaskSpan.makeCurrent()): _ =>
           try heavyTask(heavyTaskDurationSeconds * 1000)
           finally heavyTaskSpan.end()
 
+        try errorTask()
+        catch
+          case e: Throwable =>
+            println(e.getMessage)
+
+        Await.ready(future, Duration.Inf)
       finally mainSpan.end()
 
     println("Done!")
@@ -50,3 +61,32 @@ object Main:
     span.addEvent("heavy-task-end")
     // Set the status of the span
     span.setStatus(StatusCode.OK)
+
+  private def futureTask(): Future[Unit] =
+    Future:
+      val futureTaskSpan = tracer
+        .spanBuilder("future-task")
+        .startSpan()
+
+      Using(futureTaskSpan.makeCurrent()): _ =>
+        try
+          println("future task start!")
+          Thread.sleep(1000)
+          println("future task done!")
+        finally futureTaskSpan.end()
+
+  private def errorTask(): Unit =
+    val errorTaskSpan = tracer
+      .spanBuilder("error-task")
+      .startSpan()
+
+    Using(errorTaskSpan.makeCurrent()): _ =>
+      try
+        println("error task start!")
+        throw new RuntimeException("error!")
+      catch
+        case e: Throwable =>
+          // Set the status of the span
+          errorTaskSpan.setStatus(StatusCode.ERROR, e.getMessage)
+          errorTaskSpan.recordException(e)
+      finally errorTaskSpan.end()
